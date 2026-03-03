1f34b6014b0da00411e9d9740654e36b0a311f95
Badaril <120525653+Badaril@users.noreply.github.com>
2026-03-03 12:09:48 +0100

    initial

---
+++ Source/UnderPressure/PlanetaryMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PlanetaryMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDERPRESSURE_API UPlanetaryMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UnderPressure/UnderPressure.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnderPressure.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnderPressure, "UnderPressure" );
